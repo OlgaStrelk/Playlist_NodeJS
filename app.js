e84846eea6b00fbc052186e03b067c47236eb98a
Strelkova Olga <strelod@yandex.ru>
2022-06-09 15:29:30 +0300

    set absolute path

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const { generateMainView } = require('./views');
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+const { generateMainView } = require("./views");
 
 const { PORT = 3000 } = process.env;
 
 const server = http.createServer((req, res) => {
+  const dataPath = path.join(__dirname, "data.json");
   res.writeHead(200, {
-    'Content-Type': 'text/html'
+    "Content-Type": "text/html",
   });
 
   const markup = generateMainView([]);
