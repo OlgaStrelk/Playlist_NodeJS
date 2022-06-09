4a55564a5245d2233b492194b9d4ae8ae9197f0f
Strelkova Olga <strelod@yandex.ru>
2022-06-09 15:26:33 +0300

    starter pack

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const { generateMainView } = require('./views');
+
+const { PORT = 3000 } = process.env;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  const markup = generateMainView([]);
+
+  res.end(markup);
+});
+
+server.listen(PORT);
